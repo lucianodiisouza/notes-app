8c2d0f2a79508a7f1d4dd3dc28ecc72cd64ea31c
Luciano dii Souza <lucianodiisouza@hotmail.com>
2021-12-22 03:24:40 -0300

    wip form / add textarea

--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -5,7 +5,19 @@ const Form = () => {
     <form>
       <div className="form-group">
         <p className="label">Title</p>
-        <input type="text" className="input" placeholder="title" required />
+        <input type="text" className="input" placeholder="Title" required />
+      </div>
+      <div className="form-group">
+        <p className="label">Subtitle</p>
+        <input type="text" className="input" placeholder="Subtitle" required />
+      </div>
+      <div className="form-group">
+        <p className="label">Title</p>
+        <textarea
+          placeholder="type here"
+          className="textarea"
+          required
+        ></textarea>
       </div>
     </form>
   );
